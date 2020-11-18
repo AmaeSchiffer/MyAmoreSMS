11ea684f13d0cc555cbde62f0fee7a949a1a95a9
meyrussandi <andi.game92@gmail.com>
2020-11-19 02:20:57 +0700

    createpesan

--- lib/page/message_screen/components/body.dart
+++ lib/page/message_screen/components/body.dart
@@ -4,17 +4,18 @@ import 'package:amoresms/components/white_box_with_radius.dart';
 import 'package:amoresms/page/message_screen/components/autosizetextfieldcustom.dart';
 import 'package:amoresms/util/size_config.dart';
 import 'package:amoresms/views/content_of_createpesan.dart';
+import 'package:amoresms/views/content_of_detailpesan.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
-
-final inputPhoneNumberController = TextEditingController();
+  final inputPhoneNumberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
           TopImages(
             right: 0,
@@ -24,19 +25,21 @@ final inputPhoneNumberController = TextEditingController();
             left: 5,
             child: BackBtn(),
           ),
-          Positioned(
-            top: SizeConfig.screenHeight * 0.224,
-            child: WhiteBoxRadius(
-              content: ContentOfCreatePesan(),
-            ),
+          Padding(
+            padding: EdgeInsets.only(top: 200),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              //Text("sdksnkds"),
+               ContentOfCreatePesan()
+            ]),
           ),
           Positioned(
             bottom: 10,
-            child: AutoSizeTextFieldCustom(inputPhoneNumberController: inputPhoneNumberController),
+            child: AutoSizeTextFieldCustom(
+                inputPhoneNumberController: inputPhoneNumberController),
           ),
         ],
       ),
     );
   }
 }
-
